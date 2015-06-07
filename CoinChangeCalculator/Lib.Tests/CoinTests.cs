5182f50e942c90b6b2719d1d9b2123042d7a3aae
HanTeo <hteo@me.com>
2015-06-07 23:52:01 +0100

    Coin TDD implemented

--- CoinChangeCalculator/Lib.Tests/CoinTests.cs
+++ CoinChangeCalculator/Lib.Tests/CoinTests.cs
@@ -1,14 +1,93 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace CoinChangeCalculator.Tests
+namespace Lib.Tests
 {
     [TestClass]
     public class CoinTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Coin_implements_icomparable_less()
         {
+            // Arrange 
+            var coin1 = new Coin("1p", 1);
+            var coin2 = new Coin("2p", 2);
+
+            // Act
+            var res = ((IComparable<Coin>) coin1).CompareTo(coin2);
+
+            // Assert
+            Assert.AreEqual(-1, res);
+        }
+
+        [TestMethod]
+        public void Coin_implements_icomparable_greater()
+        {
+            // Arrange 
+            var coin1 = new Coin("1p", 1);
+            var coin2 = new Coin("2p", 2);
+
+            // Act
+            var res = ((IComparable<Coin>)coin1).CompareTo(coin1);
+
+            // Assert
+            Assert.AreEqual(0, res);
+        }
+
+        [TestMethod]
+        public void Coin_implements_icomparable_equals()
+        {
+            // Arrange 
+            var coin1 = new Coin("1p", 1);
+            Coin coin2 = null;
+
+            // Act
+            var res = ((IComparable<Coin>)coin1).CompareTo(coin2);
+
+            // Assert
+            Assert.AreEqual(1, res);
+        }
+
+        [TestMethod]
+        public void Coin_implements_iequatable_equals_value()
+        {
+            // Arrange 
+            var coin1 = new Coin("1p", 1);
+            var coin2 = new Coin("1p", 1);
+
+            // Act
+            var equals = ((IEquatable<Coin>)coin1).Equals(coin2);
+
+            // Assert
+            Assert.IsTrue(equals);
+        }
+
+        [TestMethod]
+        public void Coin_implements_iequatable_negative()
+        {
+            // Arrange 
+            var coin1 = new Coin("1p", 1);
+            var coin2 = new Coin("2p", 2);
+
+            // Act
+            var equals = ((IEquatable<Coin>)coin1).Equals(coin2);
+
+            // Assert
+            Assert.IsFalse(equals);
+        }
+
+        [TestMethod]
+        public void Coin_implements_iequatable_negative_denominatio()
+        {
+            // Arrange 
+            var coin1 = new Coin("1p", 1);
+            var coin2 = new Coin("something", 1);
+
+            // Act
+            var equals = ((IEquatable<Coin>)coin1).Equals(coin2);
+
+            // Assert
+            Assert.IsFalse(equals);
         }
     }
 }
--- CoinChangeCalculator/Lib/Coin.cs
+++ CoinChangeCalculator/Lib/Coin.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CoinChangeCalculator
+namespace Lib
 {
-    public class Coin
+    public class Coin : IComparable<Coin>, IEquatable<Coin>
     {
+        public int Value { get; private set; }
+
+        public string Denomination { get; private set; }
+
+        public Coin(string denomination, int value)
+        {
+            Value = value;
+            Denomination = denomination;
+        }
+
+        public Coin(Coin coin)
+        {
+            Denomination = coin.Denomination;
+            Value = coin.Value;
+        }
+
+        int IComparable<Coin>.CompareTo(Coin other)
+        {
+            if (other == null)
+                return 1;
+
+            return Value.CompareTo(other.Value);
+        }
+
+        bool IEquatable<Coin>.Equals(Coin other)
+        {
+            return Value.Equals(other.Value) && (Denomination == other.Denomination);
+        }
+
+        public override string ToString()
+        {
+            return Denomination;
+        }
     }
 }
