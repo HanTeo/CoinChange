36e2d7bea100f84fc720e52cb882f9742099f43d
hanwei <hanwei.teo@gmail.com>
2015-06-12 09:25:24 +0100

    Added a simple SpecFlow Feature Test TODO: Step Transformation

---
+++ CoinChangeCalculator/CoinChangeCalculator.Specs/CoinChangeCalculator.feature
@@ -0,0 +1,26 @@
+﻿Feature: CoinChangeCalculator
+	In order to give change
+	As a calculator
+	I need to calculate the appropriate number of coins for each available denomination of coins that adds up to the change amount
+
+Scenario: Make Correct Change
+	Given I have a target amount of 62 which to make change for
+	And I have the available coins each have values
+		|	Denomination	|	Value	|
+		|	2p				|	2			|
+		|	5p				|	5			|
+		|	10p				|	10			|
+		|	50p				|	50			|
+	And I have the denominations each are of quantities
+		|	Denomination	|	Quantity	|
+		|	2p				|	1			|
+		|	5p				|	1			|
+		|	10p				|	1			|
+		|	50p				|	1			|
+	When I calculate the change
+	Then the result should be
+		|	Denomination	|	Quantity	|
+		|	2p				|	1			|
+		|	10p				|	1			|
+		|	50p				|	1			|
+
---
+++ CoinChangeCalculator/CoinChangeCalculator.Specs/CoinChangeCalculatorSteps.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace CoinChangeCalculator.Specs
+{
+    [Binding]
+    public class CoinChangeCalculatorSteps
+    {
+        private int target;
+        private Change availableCoins;
+        private Dictionary<string, Coin> coins;
+        private Change results;
+
+        [Given(@"I have a target amount of (.*) which to make change for")]
+        public void GivenIHaveATargetAmountOfWhichToMakeChangeFor(int p0)
+        {
+            target = p0;
+        }
+
+        [Given(@"I have the available coins each have values")]
+        public void GivenIHaveTheAvailableCoinsEachHaveValues(Table table)
+        {
+            coins = new Dictionary<string, Coin>();
+            foreach (var row in table.Rows)
+            {
+                var name = row["Denomination"];
+                var value = int.Parse(row["Value"]);
+                coins.Add(name, new Coin(name, value));
+            }
+        }
+       
+
+        [Given(@"I have the denominations each are of quantities")]
+        public void GivenIHaveTheDenominationsEachAreOfQuantities(Table table)
+        {
+            availableCoins = new Change();
+            foreach (var row in table.Rows)
+            {
+                var denom = row["Denomination"];
+                var value = int.Parse(row["Quantity"]);
+                availableCoins.Add(coins[denom], value);
+            }
+        }
+
+        [When(@"I calculate the change")]
+        public void WhenICalculateTheChange()
+        {
+            target.MakeChange(availableCoins, out results);
+        }
+
+        [Then(@"the result should be")]
+        public void ThenTheResultShouldBe(Table table)
+        {
+            var expected = new Change();
+            foreach (var row in table.Rows)
+            {
+                var denom = row["Denomination"];
+                var value = int.Parse(row["Quantity"]);
+                expected.Add(coins[denom], value);
+            }
+
+            Assert.AreEqual(expected.ToString(), results.ToString());
+        }
+    }
+}
